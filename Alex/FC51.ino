9b20aea9d2e0de2ee650bbcff18e281adc4312f1
Weng-Kexin <42372486+Weng-Kexin@users.noreply.github.com>
2019-04-18 20:32:09 +0800

    Update and rename HCSR04.ino to FC51.ino

--- Alex/FC51.ino
+++ Alex/FC51.ino
@@ -6,17 +6,10 @@ void IRsetup(){
 void infraRed() {
   LEFT_OBSTACLE = analogRead(LEFT_IR);
   RIGHT_OBSTACLE = analogRead(RIGHT_IR);
-
-//  Serial.print("LEFT_OBSTACLE : ");
-//  Serial.println(LEFT_OBSTACLE);
-//  Serial.print("RIGHT_OBSTACLE : ");
-//  Serial.println(RIGHT_OBSTACLE);
   
   if (LEFT_OBSTACLE < 500 || RIGHT_OBSTACLE < 500) {
-//    Serial.println("HI HARRY");
     stop_while_forward = true;
   } else {
-//    Serial.println("BYE HARRY");
     stop_while_forward = false;
   }
 }
